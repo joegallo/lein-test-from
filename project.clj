b03fdd0de2a28f702be2ba6ff9a05bc0e96d3aca
Joe Gallo <joegallo@gmail.com>
2013-03-13 13:54:00 -0400

    add :url and :license

--- project.clj
+++ project.clj
@@ -1,3 +1,6 @@
 (defproject lein-test-from "0.1.0-SNAPSHOT"
   :description "Run your test namespaces starting from a particular namespace"
+  :url "https://github.com/joegallo/lein-test-from"
+  :license {:name "Eclipse Public License"
+            :url "http://www.eclipse.org/legal/epl-v10.html"}
   :eval-in-leiningen true)
