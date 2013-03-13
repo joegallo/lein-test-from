ddf67287843dbf88da4a586539a9c5483a26e936
Joe Gallo <joegallo@gmail.com>
2013-03-13 13:54:12 -0400

    works with lein2

--- src/leiningen/test_from.clj
+++ src/leiningen/test_from.clj
@@ -1,16 +1,14 @@
 (ns leiningen.test-from
   (:refer-clojure :exclude [test])
-  (:use [leiningen.test :only [form-for-testing-namespaces test]]
-        [robert.hooke :only [add-hook]]))
-
-(def old-form form-for-testing-namespaces)
+  (:require [leiningen.test :refer [test]]
+            [robert.hooke :refer [add-hook]]))
 
 (defn test-from [project & tests]
-  (let [from (symbol (first tests))]
-    (binding [form-for-testing-namespaces
-              (fn [nses rf & selectors]
-                (let [nses (drop-while #(not= from %) nses)]
-                  (if (seq nses)
-                    (apply old-form nses rf selectors)
-                    (require from))))]
-      (apply test project (rest tests)))))
+  (robert.hooke/add-hook #'leiningen.test/form-for-testing-namespaces
+                         (fn [f nses & args]
+                           (let [from (symbol (first tests))
+                                 nses (drop-while #(not= from %) nses)]
+                             (if (seq nses)
+                               (apply f nses args)
+                               (require from)))))
+  (apply test project (rest tests)))
